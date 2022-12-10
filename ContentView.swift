c5efce57adfebec9c0f5a260b8da4da4c94cda3e
thesameahmed <104497035+thesameahmed@users.noreply.github.com>
2022-12-10 13:33:31 -0500

    Added Starting Swift Playgrounds App Files!!
    Here we go!
    
    I'm actually opening this up in XCode instead of Swift Playgrounds. With XCode "Swift Playgrounds App" template - it is much easier to add new packages, upload new files, & get code snippets through XCode's extended functionality. 
    
    Trust me I'm the biggest Swift Playgrounds advocate in the world but XCode just has more functionality and can replicate the experience pretty well.
    
    On we go!

---
+++ ContentView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+import AudioKit
+import AudioKitUI
+import AVFoundation
+import CoreData
+
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text("""
+I said im coming for the crown - mothaf***ka - hold it down... like some buttons - I push down -
+to go and play my sounds - that I brought in - through the cloud - thats exhaustive - figure out - what it cost him - to put out all this content.
+
+""")
+            
+            
+            .multilineTextAlignment(.center)
+
+            
+            
+            
+        }
+        .padding(50)
+        .border(.black, width: 10)
+        
+    }
+}
